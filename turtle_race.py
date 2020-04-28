fd07285654a48b4e7939611123cbe7069300d785
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-04-28 22:38:18 +0530

    initial

---
+++ turtle_race.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+laura = Turtle()
+laura.color('red')
+laura.shape('turtle')
+laura.penup()
+laura.goto(-160, 100)
+laura.pendown()
+
+peter = Turtle()
+peter.color('green')
+peter.shape('turtle')
+peter.penup()
+peter.goto(-160,70)
+peter.pendown()
+
+lethal = Turtle()
+lethal.color('yellow')
+lethal.shape
