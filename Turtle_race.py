5d0dfcd5e7b30f51f996e2d8e6b8851a45205ef5
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-04-29 16:55:53 +0530

    tracls

--- Turtle_race.py
+++ Turtle_race.py
@@ -1,6 +1,14 @@
 from turtle import *
 
-
+penup()
+goto(-140,140)
 for step in range(6):
-    write(step)
-    forward(20)
+    write(step, align="center")
+    right(90)
+    forward(10)
+    pendown()
+    forward(200)
+    penup()
+    backward(210)
+    left(90)
+    forward(30)
