862e5c564690fe1eb4f5c155e232978683bc1ca5
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-04-29 17:08:33 +0530

    turtle

--- Turtle_race.py
+++ Turtle_race.py
@@ -1,8 +1,10 @@
 from turtle import *
+from random import randint
 
+speed(10)
 penup()
 goto(-140,140)
-for step in range(6):
+for step in range(16):
     write(step, align="center")
     right(90)
     forward(10)
@@ -12,3 +14,12 @@ for step in range(6):
     backward(210)
     left(90)
     forward(30)
+
+laura = Turtle()
+laura.color('red')
+laura.shape('turtle')
+laura.penup()
+laura.goto(-160,100)
+laura.pendown()
+for turn in range(100):
+    laura.forward(randint(1,5))
